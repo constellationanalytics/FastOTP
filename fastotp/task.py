701eef6833e32500a4567aecead6c362942bbb62
NLP <nlp@constellationanalytics.com>
2021-06-24 21:57:05 -0400

    fix sorting issue with tasks with random tie breaker

--- fastotp/task.py
+++ fastotp/task.py
@@ -1,10 +1,11 @@
 import os
 import structlog
+import random
 from collections import namedtuple
 
 WORKER_THREADS_PER_CORE = os.environ.get('WORKER_THREADS_PER_CORE', 128)
 
-Task = namedtuple('Task', ['priority', 'func', 'args', 'kwargs', 'log_bindings', 'blocking_perc', 'sink'])
+Task = namedtuple('Task', ['priority', 'blocking_perc', 'random', 'func', 'args', 'kwargs', 'log_bindings', 'sink'])
 
 def task_wrapper(func, args=None, kwargs=None, log_bindings=None, blocking_perc=16, sink=None, priority=1):
     args = args or ()
@@ -14,6 +15,7 @@ def task_wrapper(func, args=None, kwargs=None, log_bindings=None, blocking_perc=
         func=func,
         args=args,
         kwargs=kwargs,
+        random=random.random(),
         log_bindings=log_bindings,
         blocking_perc=blocking_perc,
         priority=priority,
